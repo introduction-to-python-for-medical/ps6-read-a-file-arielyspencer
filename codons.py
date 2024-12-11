1124d20069794a2b23af5098e029e1ec197dd173
arielyspencer <ariels2000@gmail.com>
2024-12-11 11:52:02 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,11 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    f = open(file_path)
+    rows = f.readlines()
+    f.close()
+    mydic = {}
+    for row in rows:
+      c = row.strip().split('\t')
+      mydic[c[0]] = c[2]
+    return mydic
 
 
